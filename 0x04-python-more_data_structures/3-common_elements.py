ce4972f10a94eff1b050f801c674840b31a2e85f
Au9ust <ifeanyi.hillary@gmail.com>
2022-12-11 17:42:02 +0100

    task 3 commits

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,4 @@
-
-
 #!/usr/bin/python3
-
 def common_elements(set_1, set_2):
-    if set_1 is not None and set_2 is not None:
-        return set_1 & set_2
+    """Returns common Elements in a set"""
+    return list(set_1 & set_2)
