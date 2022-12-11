5ea9f7ac8b27d508320d8016b449878c7a340c4a
Au9ust <ifeanyi.hillary@gmail.com>
2022-12-11 17:50:05 +0100

    7 update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None:
-        new_dict = a_dictionary
-        new_dict[key] = value
-        return 
+    """This add / replace key value"""
+    a_dictionary[key] = value
+    return a_dictionary 
